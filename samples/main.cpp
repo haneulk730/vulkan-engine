96aa6aaa5b8f2acff965fa332ccc2f0cfc7a005f
Haneul Kim <haneulk730@gmail.com>
2025-03-01 01:23:25 +0900

    Add VulkanApp class and Abseil-C++ submodule
    - Introduced a new `VulkanApp` class in `main.cpp` to encapsulate Vulkan application logic, including window initialization, Vulkan setup, main loop, and cleanup.
    - Updated the main function to create an instance of `VulkanApp` and call its `run` method.
    - Removed unused GLM includes and legacy window creation code.
    - Added Abseil-C++ as a submodule in `.gitmodules` and included it in the `CMakeLists.txt`.
    - Updated the `abseil-cpp` subproject to a specific commit.

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,34 +1,49 @@
 ﻿#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
-#include <iostream>
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
-int main() {
-  glfwInit();
-
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow* window =
-      glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-  uint32_t extensionCount = 0;
-  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-  std::cout << extensionCount << " extensions supported\n";
-
-  glm::mat4 matrix;
-  glm::vec4 vec;
-  auto test = matrix * vec;
-
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
+class VulkanApp {
+ public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
   }
 
-  glfwDestroyWindow(window);
+ private:
+  GLFWwindow* window;
 
-  glfwTerminate();
+  void initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+
+  void initVulkan() {}
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup() {
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+  }
+};
+
+int main() {
+  VulkanApp app;
+
+  app.run();
 
   return 0;
 }
